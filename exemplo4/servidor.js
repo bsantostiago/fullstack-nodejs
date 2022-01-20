27798ba2495152f63184cb5e508566dde3bade58
Tiago Bezerra dos Santos <bsantostiago@gmail.com>
2022-01-20 08:27:10 -0300

    Update servidor.js

--- exemplo4/servidor.js
+++ exemplo4/servidor.js
@@ -1,13 +1,18 @@
 import http from 'http';
 import fs from 'fs/promises';
 
-//const monitorRequisicao = function() { }
 const monitorRequisicao = (req, res) => { // arrow function
+    // Configurando o cabeçalho da resposta para trabalhar
+    // com arquivos HTML e charset utf-8
     res.writeHead(200, {"Content-Type" : "text/html; charset=utf8"});
 
+    // Avaliando cada requisição (cada acesso a partir de link)
     switch(req.url){
         case '/': 
+            // fs faz o carregamento e leitura do arquivo
             fs.readFile('paginas/index.html')
+
+                // Estando pronto, envia o conteudo obtido como resposta
                 .then(conteudo => res.end(conteudo));
         break;
 
@@ -25,8 +30,10 @@ const monitorRequisicao = (req, res) => { // arrow function
 }; 
 
 
+// Criamos um servidor http e indicamos a função de requisições
 const servidor = http.createServer( monitorRequisicao );
 
+// Iniciamos o servidor indicando uma porta e função (apenas como log)
 servidor.listen(8080, () => {
     console.log('Servidor rodando....');
 });
